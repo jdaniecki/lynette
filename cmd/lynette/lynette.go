682eec851ac544b36c058bac1482b3a6783165ba
Józef Daniecki <jozek.daniecki@gmail.com>
2023-10-22 12:58:49 +0200

    Setup container hostname. (#5)

--- cmd/lynette/lynette.go
+++ cmd/lynette/lynette.go
@@ -15,7 +15,9 @@ limitations under the License.
 */
 package main
 
-import "github.com/jdaniecki/lynette/internal/cmd"
+import (
+	"github.com/jdaniecki/lynette/internal/cmd"
+)
 
 func main() {
 	cmd.Execute()
